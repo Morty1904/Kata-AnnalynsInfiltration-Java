ccc270beced3d21ebbd87ce92b49b7277b83841a
Morty1904 <oscarendercraft@gmail.com>
2024-11-04 23:12:25 +0100

    finished the methods

--- src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
+++ src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
@@ -3,20 +3,18 @@ package org.factoriaf5.infiltration;
 public class AnnalynsInfiltration {
 
     public static boolean canFastAttack(boolean knightIsAwake) {
-        // Implement method
+        return !knightIsAwake;
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        // Implement method
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        // Implement method
+        return prisonerIsAwake && !archerIsAwake;
     }
 
-    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
-            boolean petDogIsPresent) {
-        // Implement method
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        return petDogIsPresent ? !archerIsAwake : prisonerIsAwake && !knightIsAwake && !archerIsAwake;
     }
-
 }
